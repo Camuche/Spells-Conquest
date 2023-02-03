fd8ed66f9753a3b27ebb71bdc9fe248d3db3b733
rezioz <ungureanunicolas@gmail.com>
2023-02-03 13:07:36 +0100

    scene restart when player die

--- Assets/script/PlayerController.cs
+++ Assets/script/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -290,7 +291,7 @@ public class PlayerController : MonoBehaviour
     {
         if (life <= 0)
         {
-            Destroy(gameObject);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 
