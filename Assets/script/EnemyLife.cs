c53723f38160f2084fb73e27e2067dd4825cfa17
rezioz <ungureanunicolas@gmail.com>
2023-02-03 12:22:10 +0100

    added enemi basic health bar

--- Assets/script/EnemyLife.cs
+++ Assets/script/EnemyLife.cs
@@ -5,20 +5,41 @@ using UnityEngine;
 public class EnemyLife : MonoBehaviour
 {
 
-    public int life;
+    public float life;
+    public bool DisplayLife;
+    GameObject healthBar;
+
+    private float maxLife;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        healthBar = transform.Find("HealthBar").gameObject;
+
+        if (DisplayLife)
+        {
+            healthBar.GetComponent<CanvasGroup>().alpha = 1;
+        }
+        else
+        {
+            healthBar.GetComponent<CanvasGroup>().alpha = 0;
+        }
+
+
+        maxLife = life;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        drawHealth();
     }
 
+
+    private void drawHealth()
+    {
+        healthBar.transform.Find("ImageGreen").GetComponent<RectTransform>().localScale = new Vector3((float)(life/maxLife),1,1);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -32,4 +53,6 @@ public class EnemyLife : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+
 }
