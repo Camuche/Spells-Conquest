d6ed3b947f83cb18a52b8365b01675274a514a84
Sofiane Kaci <sofiane.kaci@mail-ecv.fr>
2023-02-12 16:17:25 +0100

    PressurePlateFeedback

--- Assets/script/AnimationPressurePlate.cs
+++ Assets/script/AnimationPressurePlate.cs
@@ -2,20 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimationStairs : MonoBehaviour
+public class AnimationPressurePlate : MonoBehaviour
 {
-
+    
     public Transform target;
+    public GameObject obstacle;
+
+    private Vector3 posInit;
     public float speed;
+    bool isTrigger = false;
+
+
+    public GameObject rendererGameObject;
+    public float speedRenderer;
+    Vector3 targetRenderer;
+    Vector3 posInitRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        posInit = obstacle.transform.position;
+
+        posInitRenderer = rendererGameObject.transform.position;
+        targetRenderer = new Vector3(rendererGameObject.transform.position.x, rendererGameObject.transform.position.y - (rendererGameObject.transform.localScale.y )+0.01f, rendererGameObject.transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, target.position, 0.01f*speed);
+        if (isTrigger == true)
+        {
+            obstacle.transform.position = Vector3.MoveTowards(obstacle.transform.position, target.position, 0.01f * speed);
+            rendererGameObject.transform.position = Vector3.MoveTowards(rendererGameObject.transform.position, targetRenderer , 0.01f* speedRenderer);
+        }
+        else
+        {
+            obstacle.transform.position = Vector3.MoveTowards(obstacle.transform.position, posInit, 0.01f * speed);
+            rendererGameObject.transform.position = Vector3.MoveTowards(rendererGameObject.transform.position, posInitRenderer, 0.01f * speedRenderer);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        isTrigger = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isTrigger = false;
     }
 }
