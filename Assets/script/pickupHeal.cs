070bc828f8ca5132d204774f1ea00f8649e72c0d
Rezioz <ungureanunicolas@gmail.com>
2023-02-16 16:52:49 +0100

    added healing pickup

---
+++ Assets/script/pickupHeal.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickupHeal : MonoBehaviour
+{
+
+    bool canPickUp = false;
+    [SerializeField]float amount;
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canPickUp && Input.GetButtonDown("Interact"))
+        {
+            player.GetComponent<PlayerController>().life += amount;
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            player = other.gameObject;
+            canPickUp = true;
+        }
+
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            canPickUp = false;
+        }
+
+
+    }
+}
