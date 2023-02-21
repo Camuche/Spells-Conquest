7ce85330367cecc8631d0ada1b5e3548f4e0903e
Sofiane Kaci <sofiane.kaci@mail-ecv.fr>
2023-02-21 18:30:28 +0100

    FireTorchHaveNewVisuals

---
+++ Assets/prefabs/FireProtection/FireProtectionStart.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireProtectionStart : MonoBehaviour
+{
+
+
+    [SerializeField] Renderer fireProtection1;
+    [SerializeField] Renderer fireProtection2;
+    [SerializeField] Renderer fireProtection3;
+
+    private float startFloat = -1;
+    [SerializeField] float speed;
+
+    private bool boolEnable = false;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+        if (boolEnable == true)
+        {
+            if (startFloat < 1)
+            {
+                startFloat += speed * Time.deltaTime;
+
+            }
+            else startFloat = 1;
+
+
+            fireProtection1.material.SetFloat("_Start", startFloat);
+            fireProtection2.material.SetFloat("_Start", startFloat);
+            fireProtection3.material.SetFloat("_Start", startFloat);
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Fireball")
+        { 
+            boolEnable = true;
+        }
+    }
+}
