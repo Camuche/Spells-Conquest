fb4d9ad0ccba6e5ab8a4d548d5ca938fb56be776
Rezioz <unureanunicolas@gmail.com>
2023-03-14 18:34:25 +0100

    [WIP] added heavy enemi

---
+++ Assets/script/IgnoreWave.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class IgnoreWave : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.Find("Wave(Clone)") != null)
+        {
+            Physics.IgnoreCollision(gameObject.GetComponent<CapsuleCollider>(), GameObject.Find("Wave(Clone)").GetComponent<BoxCollider>());
+            Physics.IgnoreCollision(gameObject.GetComponent<CapsuleCollider>(), GameObject.Find("Wave(Clone)").GetComponentInChildren<BoxCollider>());
+            Physics.IgnoreCollision(gameObject.GetComponent<CapsuleCollider>(), GameObject.Find("Wave(Clone)").GetComponent<CharacterController>());
+
+        }
+    }
+}
