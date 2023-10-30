2551a16c1588e4a81463560fb74e17d47cfc3e4d
Sofiane Kaci <sofiane.kaci@mail-ecv.fr>
2023-10-30 11:43:44 +0100

    few changes & fix billboard

--- Assets/script/BillboardFX.cs
+++ Assets/script/BillboardFX.cs
@@ -7,18 +7,15 @@ public class BillboardFX : MonoBehaviour
 
     Transform camTransform;
 
-    Quaternion originalRotation;
+    
 
     void Start()
     {
-        originalRotation = transform.rotation;
         camTransform = Camera.main.transform;
-
     }
 
     void Update()
     {
-
-        transform.rotation = camTransform.rotation * originalRotation;
+        transform.rotation = Quaternion.LookRotation(transform.position - camTransform.position);
     }
 }
