1bada5f769f590887929bbd1393b5853ab8b992c
Sofiane Kaci <sofiane.kaci@mail-ecv.fr>
2023-11-29 18:09:33 +0100

    Money start

---
+++ Assets/script/Money.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Money : MonoBehaviour
+{
+    [SerializeField] float speed, speedTowardPlayer, timeFloating;
+    Vector3 targetPosition;
+    float randomRange;
+    bool followPlayer = false;
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomRange = Random.Range(0.5f, 2f);
+        targetPosition = transform.position + Vector3.up * randomRange;
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!followPlayer)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, 0.01f * speed * randomRange);
+        }
+        
+
+        if (transform.position == targetPosition)
+        {
+            followPlayer = true;
+        }
+
+        if (followPlayer)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, 0.01f * speedTowardPlayer );
+        }
+    }
+}
