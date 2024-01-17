f4535542f21975c2c7e4a6a24cfa7000b38a48ab
Sofiane Kaci <sofiane.kaci@mail-ecv.fr>
2024-01-17 14:15:09 +0100

    Enemy dash v2 wip

--- Assets/script/Enemy/EnemyDash.cs
+++ Assets/script/Enemy/EnemyDash.cs
@@ -10,8 +10,12 @@ public class EnemyDash : MonoBehaviour
     NavMeshAgent navMeshAgent;
     public float speed, dashRange;
     bool isCharging;
+    public float chargeAnimationTime;
+    public int raycastIterations, raycastDist;
 
     [SerializeField] float range;
+    bool rotate;
+
 
     // Start is called before the first frame update
     void Start()
@@ -24,7 +28,12 @@ public class EnemyDash : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(player.transform.position, transform.position) <= range && Vector3.Distance(player.transform.position, transform.position) >= dashRange && !isCharging)// && hit.GameObject.tag == "Player")
+        if (rotate == true)
+        {
+            gameObject.transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, player.transform.position - transform.position, GetComponent<EnemyFollower>().rotationSpeed * Time.deltaTime, 0.0f));
+        }
+
+        if (Vector3.Distance(player.transform.position, transform.position) <= range && Vector3.Distance(player.transform.position, transform.position) >= dashRange && !isCharging)// && hit.GameObject.tag == "Player")
         {   
             RaycastHit hit;
             Physics.Raycast(player.transform.position,(transform.position - player.transform.position).normalized, out hit,range);
@@ -42,10 +51,71 @@ public class EnemyDash : MonoBehaviour
         { //DO ONCE
             navMeshAgent.SetDestination(transform.position);
             isCharging = true;
-            Debug.Log("CHAARGE");
-        }
+            gameObject.transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, player.transform.position - transform.position, GetComponent<EnemyFollower>().rotationSpeed * Time.deltaTime, 0.0f));
 
-        
-        
+            //Make animations
+            /*for (int i=0; i < raycastIterations; i++)
+            {
+                RaycastHit hit;
+                Physics.Raycast(transform.position + raycastDist * i * Vector3.forward, Vector3.down, out hit, Mathf.Infinity);
+                //Instantiate(cylinder, hit.point.transform);
+                navMeshAgent.SetDestination(hit.point);
+            }*/
+            Invoke("Dash", chargeAnimationTime);
+        }  
     }
+
+    public float dashspeedMultiplication;
+
+    Vector3 dashDest;
+    void Dash()
+    {
+        Debug.Log("CHAARGE");
+        navMeshAgent.speed *= dashspeedMultiplication;
+        //navMeshAgent.acceleration = 100;
+        gameObject.transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, player.transform.position - transform.position, GetComponent<EnemyFollower>().rotationSpeed * Time.deltaTime, 0.0f));
+        for (int i=0; i < raycastIterations; i++)
+        {
+            RaycastHit hit;
+            Physics.Raycast(transform.position + raycastDist * (i+1) * transform.forward, Vector3.down, out hit, Mathf.Infinity);
+            //Instantiate(cylinder, hit.transform);
+            //Debug.Log(hit.collider);
+            //Debug.Log(hit.transform.gameObject.layer);
+            //Debug.Log(LayerMask.NameToLayer("ground"));
+            
+
+            if(hit.collider == null || hit.transform.gameObject.layer != LayerMask.NameToLayer("ground"))
+            {
+                return;
+            }
+            dashDest = hit.point;
+            //Debug.Log(i);
+            //Debug.Log(hit.point);
+            //Debug.Log(dashDest);
+        }
+        navMeshAgent.SetDestination(dashDest);//+Vector3.up);
+        //navMeshAgent.speed *= dashspeedMultiplication;
+        //Invoke("Decelerate", 0.2f);
+        Invoke("Rotate", timeStuck);
+    }
+
+    public float timeStuck;
+
+    void Rotate()
+    {
+        rotate = true;
+        Invoke("StopBeingStuck", 1);
+    }
+    void StopBeingStuck()
+    {
+        //navMeshAgent.acceleration = 16;
+        //gameObject.transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, player.transform.position - transform.position, GetComponent<EnemyFollower>().rotationSpeed * Time.deltaTime, 0.0f));
+        rotate = false;
+        isCharging = false;
+    }
+
+    /*void Decelerate()
+    {
+        navMeshAgent.acceleration = 16;
+    }*/
 }
