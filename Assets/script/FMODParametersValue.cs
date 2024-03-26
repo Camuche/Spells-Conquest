d6e371d2d12351342ea385042327360a6b68c476
Sofiane Kaci <sofiane.kaci@mail-ecv.fr>
2024-03-26 12:08:12 +0100

    FMOD integration Update

---
+++ Assets/script/FMODParametersValue.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FMODParametersValue : MonoBehaviour
+{
+    //private FMOD.Studio.EventInstance instance;
+
+    /*[FMODUnity.EventRef] public string fmodEvent;
+
+    [SerializeField] [Range(0,100)] private float playerLife;
+    [SerializeField] *//*[Range(0,100)]*//* private float value2;
+    [SerializeField] *//*[Range(0,100)]*//* private float value3;
+
+
+    void Start()
+    {
+        instance = FMODUnity.RuntimeManager.CreateInstance(fmodEvent);
+        instance.start();
+    }
+
+
+    void Update()
+    {
+        instance.setParameterByName("playerLife FMODname", playerLife);
+        instance.setParameterByName("value2 FMODname", value2);
+        instance.setParameterByName("value3 FMODname", value3);
+    }*/
+}
