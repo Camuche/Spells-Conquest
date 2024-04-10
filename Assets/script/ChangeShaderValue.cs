563fc95aa6bd1ee72bc3de3be3da3d9eb94f8be1
Sofiane Kaci <sofiane.kaci@mail-ecv.fr>
2024-04-10 13:13:59 +0200

    Update heal & burning vfx + mouse sound adaptation

---
+++ Assets/script/ChangeShaderValue.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeShaderValue : MonoBehaviour
+{
+    public GameObject materialGameObject;
+    public string shaderValueName;
+    public float startValue, endValue, speed;
+    public bool destroyGameObjectAtEnd;
+    public GameObject destroyGameObject;
+
+    float shaderValue;
+    bool changeValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shaderValue = startValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (changeValue)
+        {
+            shaderValue += Time.deltaTime * speed;
+            materialGameObject.GetComponent<MeshRenderer>().material.SetFloat(shaderValueName,shaderValue);
+            if (destroyGameObjectAtEnd && shaderValue >= endValue)
+            {
+                Destroy(destroyGameObject);
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Fireball" || other.tag == "FireShield")
+        {
+            changeValue = true;
+        }
+    }
+}
